a4b7fa11877fbe24ec8f3965afd54d8707b2ab39
Jagjot Singh <jagjot@qwibzio.com>
2018-08-14 03:19:04 +0530

    Added realtime board updation code and added leaderboards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   def index
     session[:user] ||= SecureRandom.hex(6)
     @user = session[:user]
+    @top_users = Box.where('user != "NA"').group(:user).select('COUNT(user) number, user user').order('number DESC').limit(3)
+    @top_colors = []
+    @top_users.each do |user|
+      color = Box.where(user: user.user).group(:color).select('COUNT(color) number, color color').order('number DESC').limit(3)
+      @top_colors << color
+    end
   end
 end
