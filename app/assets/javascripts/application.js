d66caff86517609f4a47f55590b6f44f3d129cab
Jagjot Singh <jagjot@qwibzio.com>
2018-08-13 18:59:33 +0530

    Added JS to show colors on boxes when updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,34 +17,37 @@
 
 
 $(function() {
-    let i = 1, j = 1, count = 1;
+    let i, j, count = 1;
     let $main_table = $('#main-table');
-    for(i = 1; i <= 20; i++) {
-        $main_table.append('<tr id="row-' + i + '"></tr>');
-        let $current_row = $('#row-' + i);
-        for(j = 0; j < 20; j++) {
-            $current_row.append('<td id="box-' + count + '" class="box" data-number="' + count + '"></td>');
-            count++;
+    let $current_row;
+    $.get('/boxes.json').then(function(resp) {
+        for(i = 1; i <= 20; i++) {
+            $main_table.append('<tr id="row-' + i + '"></tr>');
+            $current_row = $('#row-' + i);
+            for(j = 1; j <= 20; j++) {
+                $current_row.append('<td id="box-' + count + '" class="box" data-number="' + count + '" style="background-color: ' + resp[count - 1].color + '"></td>');
+                count++;
+            }
         }
-    }
+    });
 
-    $('.box').on('click', function() {
-       let number = $(this).data('number');
-       let color = $('#color').val();
+    $('body').on('click', 'td.box', function() {
+        let number = $(this).data('number');
+        let color = $('#color').val();
 
-       $.ajax('/boxes/' + number + '.json', {
-           data: JSON.stringify({
-               color: color
-           }),
-           dataType: 'JSON',
-           method: 'PATCH',
-           headers: {
-               'Content-Type': 'application/json'
-           }
-       }).then(function(resp) {
-           if(resp.status === true) {
-               $('#box-' + number).attr('style', 'background-color: ' + color);
-           }
-       })
+        $.ajax('/boxes/' + number + '.json', {
+            data: JSON.stringify({
+                color: color
+            }),
+            dataType: 'JSON',
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function(resp) {
+            if(resp.status === true) {
+                $('#box-' + number).attr('style', 'background-color: ' + color);
+            }
+        })
     });
 });
